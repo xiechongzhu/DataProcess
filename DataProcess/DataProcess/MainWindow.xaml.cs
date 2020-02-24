ea36076135a4879d8a288a5ac92f7b2cb6b504a8
xiechongzhu <2936281@qq.com>
2020-02-24 19:11:03 +0800

    first commit

---
+++ DataProcess/DataProcess/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace DataProcess
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TestInfo testInfo;
+        private UdpClient udpClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            TestInfoWindow testInfoWindow = new TestInfoWindow();
+            testInfoWindow.Owner = this;
+            if (!(bool)testInfoWindow.ShowDialog())
+            {
+                return;
+            }
+
+            testInfo = testInfoWindow.GetTestInfo();
+
+            try
+            {
+                udpClient = new UdpClient(int.Parse(editPort.Text));
+                udpClient.JoinMulticastGroup(IPAddress.Parse(editIpAddr.Text));
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("加入组播组失败:" + ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            editIpAddr.IsEnabled = false;
+            editPort.IsEnabled = false;
+            udpClient.BeginReceive(EndReceive, null);
+        }
+
+        private void EndReceive(IAsyncResult ar)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+            try
+            {
+                byte[] recvBuffer = udpClient?.EndReceive(ar, ref endPoint);
+                udpClient.BeginReceive(EndReceive, null);
+            }
+            catch (Exception)
+            { }
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            udpClient.DropMulticastGroup(IPAddress.Parse(editIpAddr.Text));
+            udpClient.Close();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            editIpAddr.IsEnabled = true;
+            editPort.IsEnabled = true;
+        }
+    }
+}
---
+++ DataProcess/DataProcess/TestInfoWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataProcess
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class TestInfoWindow : Window
+    {
+        public TestInfoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if(editName.Text.Equals(String.Empty) || editOperator.Text.Equals(String.Empty))
+            {
+                MessageBox.Show("实验名称和人员不能为空", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            DialogResult = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        public TestInfo GetTestInfo()
+        {
+            return new TestInfo
+            {
+                TestName = editName.Text,
+                Operator = editOperator.Text,
+                Comment = editComment.Text,
+                TestTime = DateTime.Now
+            };
+        }
+    }
+}
