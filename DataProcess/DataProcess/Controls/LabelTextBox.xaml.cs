f952f4ffe563bb3a7a061efe22797e5e7e5bbf4e
Antoino-x <281721237@qq.com>
2021-01-19 09:56:21 +0800

    更新界面
    更新界面。遥测数据显示整合

---
+++ DataProcess/DataProcess/Controls/LabelTextBox.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataProcess.Controls
+{
+    /// <summary>
+    /// Interaction logic for LabelTextBox.xaml
+    /// </summary>
+    public partial class LabelTextBox : UserControl
+    {
+        public LabelTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataProcess/DataProcess/CustomControl/LabelTextBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ScoreTools.CustomControl
+{
+    public class LabelTextBox : TextBox
+    {
+        static LabelTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LabelTextBox), new FrameworkPropertyMetadata(typeof(LabelTextBox)));
+        }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(Object), typeof(LabelTextBox), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty LabelPositionProperty = DependencyProperty.Register("LabelPosition", typeof(Position), typeof(LabelTextBox), new PropertyMetadata(Position.Top));
+
+        public Object Label
+        {
+            get { return (Object)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public Position LabelPosition
+        {
+            get { return (Position)GetValue(LabelPositionProperty); }
+            set { SetValue(LabelPositionProperty, value); }
+        }
+
+    }
+
+    public enum Position
+    {
+        Top,
+        Left
+
+    }
+}
+
