c158cb835d8f9ce9dfcf4b1fe12ae7879955d105
xiechongzhu <2936281@qq.com>
2021-04-15 19:12:42 +0800

    commit

--- DataProcess/DataProcess/YaoCe/GenericFunction.cs
+++ DataProcess/DataProcess/YaoCe/GenericFunction.cs
@@ -71,14 +71,14 @@ namespace DataProcess
                 if ((lab.Name != "XiTong_NeiBuKongZhiDian") && (lab.Name != "XiTong_GongLvDian"))
                 {
                     //Console.WriteLine("2:{0}", lab.Name);
-                    if (lab.Text.Contains("正常") || lab.Text.Contains("无效"))
+                    if (lab.Text.Contains("正常") || lab.Text.Contains("有效"))
                     {
                         System.Windows.Media.Color color = System.Windows.Media.Color.FromArgb(30, 0, 255, 0);
                         System.Windows.Media.SolidColorBrush solidColorBrush = new System.Windows.Media.SolidColorBrush(color);
                         System.Windows.Media.Brush brushes = solidColorBrush;
                         lab.Background = brushes; //     // 第1个参数为透明度(alpha)参数,其后为红,绿和蓝
                     }
-                    else if (lab.Text.Contains("异常") || lab.Text.Contains("有效"))
+                    else if (lab.Text.Contains("异常") || lab.Text.Contains("无效"))
                     {
                         System.Windows.Media.Color color = System.Windows.Media.Color.FromArgb(30, 255, 0, 0);
                         System.Windows.Media.SolidColorBrush solidColorBrush = new System.Windows.Media.SolidColorBrush(color);
@@ -102,14 +102,14 @@ namespace DataProcess
 
                 if((lab.Name != "XiTong_NeiBuKongZhiDian") && (lab.Name != "XiTong_GongLvDian"))
                 {
-                    if (lab.Text.Contains("正常") || lab.Text.Contains("无效"))
+                    if (lab.Text.Contains("正常") || lab.Text.Contains("有效"))
                     {
                         System.Windows.Media.Color color = System.Windows.Media.Color.FromArgb(30, 0, 255, 0);
                         System.Windows.Media.SolidColorBrush solidColorBrush = new System.Windows.Media.SolidColorBrush(color);
                         System.Windows.Media.Brush brushes = solidColorBrush;
                         lab.Background = brushes; //     // 第1个参数为透明度(alpha)参数,其后为红,绿和蓝
                     }
-                    else if (lab.Text.Contains("异常") || lab.Text.Contains("有效"))
+                    else if (lab.Text.Contains("异常") || lab.Text.Contains("无效"))
                     {
                         System.Windows.Media.Color color = System.Windows.Media.Color.FromArgb(30, 255, 0, 0);
                         System.Windows.Media.SolidColorBrush solidColorBrush = new System.Windows.Media.SolidColorBrush(color);
